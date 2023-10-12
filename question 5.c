9a5e532dc795d43b9408e7b659fcfdd2b84049e9
BabkenK <139691909+BabkenK@users.noreply.github.com>
2023-10-12 20:14:50 +0400

    Գրեք ֆունկցիա, որը ցուցիչների միջոցով փոխում է երկու ամբողջ թվով փոփոխականների արժեքները:
    5.Գրեք ֆունկցիա, որը ցուցիչների միջոցով փոխում է երկու ամբողջ թվով փոփոխականների արժեքները:

---
+++ question 5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int x, y;
+    printf("Enter the first number: ");
+    scanf("%d", &x);
+    printf("Enter the second number: ");
+    scanf("%d", &y);
+
+    swap(&x, &y);
+
+    printf("Swap: x = %d, y = %d\n", x, y);
+
+    return 0;
+}
